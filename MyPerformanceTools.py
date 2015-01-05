ef782711a41a163d56b4b79a764d1c83c7450dc7
HiImJayHireMe <jjtolton@gmail.com>
2015-01-05 14:58:25 -0500

    unpack handles strings; unpack string_safe mode added

--- MyPerformanceTools.py
+++ MyPerformanceTools.py
@@ -12,6 +12,7 @@ Created on Thu Jan 01 17:40:08 2015
 """
 
 from MyDevTools.MyDecorator import decorator
+from MyDevTools.MyDataTypes import histogram, get_histogram, loop
 
 __all__ = [
 
@@ -24,7 +25,11 @@ __all__ = [
 
 def main():
     stuff = [range(10) for x in range(10)]
-    print unpack(stuff)
+    stuff2 = [3,(1,2,3),(22,45,20),[range(7)]]
+    print unpack(*stuff)
+    print unpack(*stuff2)
+    print unpack('five', 'four', 'three', (22,5,10))
+    print unpack('five', 'four', 'three', (22,5,10), string_safe=False)
 
 @decorator
 def memo(f):
@@ -62,40 +67,7 @@ def combined_gen(args, non_redundant=True):
         for arg in args:
             yield arg.next()
             
-def get_histogram(iterable):
-    """Returns histogram of iterable"""
-    
-    hist = dict()    
-    for item in iterable:
-        try:
-            hist[item] += 1
-        except KeyError:
-            hist[item] = 1
-    return hist
-    
-class histogram(dict):
-    
-    def push(self,item):
-        try:
-            self[item] += 1
-        except KeyError:
-            self[item] = 1
 
-    def push_all(self,iterable):
-        """Returns histogram of iterable"""
-        
-        try:
-            assert isinstance(iterable, (list, tuple))
-        except AssertionError:
-            "Histogram must be composed of finite list"
-            raise AssertionError
-                
-        for item in iterable:
-            try:
-                self[item] += 1
-            except KeyError:
-                self[item] = 1
-        return self
 
 @decorator
 def n_ary(f):
@@ -105,19 +77,33 @@ def n_ary(f):
         return x if not args else f(x, n_ary_f(*args))
     return n_ary_f
 
-def unpack(items):
+def unpack(*items, **kw):
+    
+    string_safe = kw.get('string_safe', True)
+    assert isinstance(string_safe, bool)
+    
+    def intList(n):
+        return [n] if isinstance(n, (int,float)) else n
+    items = map(intList, items)
+        
     def _unpack(items):
         try:
     #        print items[0], len(items[0])
-            return (items if len(items[0]) <= 1 
+            return (items if isinstance(item, str) or len(items[0]) <= 1
                 else unpack(items[0][:1]) + unpack(items[0][1:]))
         except TypeError:
             return items
     cache = []
-    for item in items:
-        cache += _unpack(item)
-    return cache
-        
+    try:
+        for item in items:
+            if isinstance(item, str) and string_safe:
+                cache += [item]
+            else:
+                cache += _unpack(item)
+        return cache
+    except:
+        return items
+            
 
 
 if __name__=='__main__':
